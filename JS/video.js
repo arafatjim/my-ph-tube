4b0f6e1a9d0006280dca8bf5d71647cbde507fb7
arafatjim <jimyeasirarafat28@gmail.com>
2025-03-11 01:18:33 +0600

    add cards

--- JS/video.js
+++ JS/video.js
@@ -24,31 +24,37 @@ const displayCategories = (categories) => {
 loadCategories();
 
 
-let videos = [];
-const loadVideos = async (category) => {
-          fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-          .then((response) => response.json())
-          .then(data => displayVideos(data.videos))
-          .catch(error => console.log(error));
-};
-loadVideos();
-const displayVideos = (videos) => {
-          const videoContainer=document.getElementById("video-container");
-          videos.forEach((item) =>{
-                    const card=document.createElement("div");
-                    card.classList="card";
-                    card.innerHTML=`
-                    <div class="card flex w-11/12 border-2 border-black-500 " style="">
-                    <img src="${item.thumbnail}" class="card-img-top w-[20em] rounded-3" alt="...">
-                    <div class="card-body " alt="..."">
-                    <h5 class="card-title  
-                    text-[#3d3d3d] text-center font-bold text
-                    ">${item.title}</h5>
-                    <p class="card-text w-[10em]  ">${item.description}</p>
-                    </div>
-                    </div>
-                    `;
-                    videoContainer.appendChild(card);
 
-          })
-};
+const loadVideos=async(videos) => {
+  try{
+    const response=await fetch("https://openapi.programming-hero.com/api/phero-tube/videos");
+    const data=await response.json();
+    videos=data.videos;
+    displayVideos(videos);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+const displayVideos = (videos) => {
+  const videoContainer=document.getElementById("video-container");
+  videoContainer.innerHTML='';
+  videos.forEach((item)=>{
+    const cards=document.createElement("div");
+    cards.classList="card";
+    cards.innerHTML=`
+    <div class="card border-2 gap-6 bg-[#dfe4ea] height-[250px]" style="">
+    <img src="${item.thumbnail}" class="card-img-top height-[150px] rounded-md" alt="...">
+    <div class="card-body " alt="..."">
+    <h5 class="card-title text-[#3d3d3d] text-center font-bold text-lg
+    ">${item.title}</h5>
+    <p class="card-text">${item.description}</p>
+    </div>
+    </div>
+    `;
+    videoContainer.appendChild(cards);
+
+  })
+}
+loadVideos();
